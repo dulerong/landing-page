a6398ae9f25b52be7f2ae0097cd76a074621dd5a
Codey Du <codeydu@hotmail.com>
2020-05-06 11:58:01 +0900

    Updates

---
+++ precache-manifest.f0672a8469ef0caed749fbffd3b3fc37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a364a728535683460122379b9b19a93",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "990fb2a61478df178fe7",
+    "url": "/landing-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "757213fa95b76900a34a",
+    "url": "/landing-page/static/js/2.72b1dba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page/static/js/2.72b1dba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990fb2a61478df178fe7",
+    "url": "/landing-page/static/js/main.b68237b0.chunk.js"
+  },
+  {
+    "revision": "a9b109b84df73566fc20",
+    "url": "/landing-page/static/js/runtime-main.bbd2f894.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landing-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
